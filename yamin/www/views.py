fc0f10af7f3bca12ebb2c57d5c49b940037a5297
PyeongYun Myeong <audvuddbs@gmail.com>
2020-07-20 14:51:47 +0900

    Update views.py

--- yamin/www/views.py
+++ yamin/www/views.py
@@ -8,8 +8,8 @@ def index(request):
 
 def hangul_split():
     """
-    Hangul Jamo Unicode = 자음, 모음 영역 (0x1100 ~ 0x11FF)
-    Hangul Syllables Unicode (가~힣) = 한글 음절 영역 (0xAC00 ~ 0xD7A3)
+    - Hangul Jamo Unicode = 자음, 모음 영역 (0x1100 ~ 0x11FF)
+    - Hangul Syllables Unicode (가~힣) = 한글 음절 영역 (0xAC00 ~ 0xD7A3)
 
     - 초성 (19개) : ㄱ, ㄲ, ㄴ, ㄷ, ㄸ, ㄹ, ㅁ, ㅂ, ㅃ, ㅅ, ㅆ, ㅇ, ㅈ, ㅉ, ㅊ, ㅋ, ㅌ, ㅍ, ㅎ
     - 중성 (21개) : ㅏ, ㅐ, ㅑ, ㅒ, ㅓ, ㅔ, ㅕ, ㅖ, ㅗ, ㅘ, ㅙ, ㅚ, ㅛ, ㅜ, ㅝ, ㅞ, ㅟ, ㅠ, ㅡ, ㅢ, ㅣ
